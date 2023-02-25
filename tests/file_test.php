60dfe943318e89e872968df3f38ab0e7ffecce9a
Netkas <zixing.narrakas1996@gmail.com>
2023-02-25 00:53:58 -0500

    Added test

---
+++ tests/file_test.php
@@ -0,0 +1,14 @@
+<?php
+
+    require 'ncc';
+    import('net.nosial.tempfile');
+
+    $temp = new \TempFile\TempFile('bin');
+    print(sprintf('Tempfile: %s', $temp->getFilepath()) . PHP_EOL);
+
+    file_put_contents($temp, 'Hello, world!');
+    print(sprintf('Filesize: %s', filesize($temp->getFilepath())) . PHP_EOL);
+
+    sleep(10);
+    print('Exiting...' . PHP_EOL);
+    exit(0);
